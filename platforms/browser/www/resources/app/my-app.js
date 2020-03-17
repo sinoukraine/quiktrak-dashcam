cfc1792e76931a5c693e9d8231c2a2f5623542a3
vladbiletski <vladbiletski@gmail.com>
2020-03-18 00:38:59 +0200

    kjk

--- platforms/browser/www/resources/app/my-app.js
+++ platforms/browser/www/resources/app/my-app.js
@@ -401,7 +401,7 @@ var App = new Framework7({
 									
 									
 									
-									var lp = "file:///data/user/0/com.quiktrak.quiktrak_dashcam/files/" + fileName;//// //'file:///storage/emulated/0/Download/' + fileName;'file:///data/user/0/com.quiktrak.quiktrak_dashcam/files/files/' + folder + '/' + fileName;
+									var lp = 'file:///storage/emulated/0/Download/' + fileName;//// //'file:///storage/emulated/0/Download/' + fileName;'file:///data/user/0/com.quiktrak.quiktrak_dashcam/files/files/' + folder + '/' + fileName;
 									var rp = '/storage/sdcard1/DVRMEDIA/CarRecorder/'+type+'/'+folderDate+'/'+fileName;
 									
 									//App.dialog.alert(lp);								
